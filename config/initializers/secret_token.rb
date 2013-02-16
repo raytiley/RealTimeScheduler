9b4fb0d26b2bea42a6c6f12a69d84a5dce8eee18
Ray Tiley <raytiley@gmail.com>
2013-02-16 09:45:24 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealTimeScheduler::Application.config.secret_token = 'b4ce26266527e6758389699a87c287e175c623155a543ddd23605ba74ce7f87582718f66069e9168d8d64d1f09ab4a35df34a594e671672cf106ccbed4e2875e'
