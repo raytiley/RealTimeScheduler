b5d3cd2bf890e6d43b6dfcfc8bbc3c837cbcfc55
Ray Tiley <raytiley@gmail.com>
2013-02-17 08:25:04 -0600

    implemented fixed priority deadline monotonic scheduleing

--- app/models/task_set.rb
+++ app/models/task_set.rb
@@ -1,5 +1,105 @@
 class TaskSet < ActiveRecord::Base
-  attr_accessible :name
-
+  attr_accessible :name, :tasks
   has_many :tasks
+
+  def hyper_period
+  	# hyper perid = lowest common multiple of all periods in task set
+  	periods = self.tasks.pluck(:period)
+  	periods.inject(:lcm)
+  end
+
+  def get_released_jobs(time)
+  	# find all the tasks releaed at given time
+  	tasks = self.tasks.select {|task| (time - task.offset) % task.period == 0}
+
+  	# return array of  hashes containing task and deadline
+  	tasks.map do |task|
+  		{:task => task, :deadline => time + (task.deadline - task.offset)}
+  	end
+  end
+
+  def get_highest_priorty_task(jobs)
+  	#sort tasks by deadline and then return first task
+  	jobs.sort!{|a, b| a[:task].deadline <=> b[:task].deadline }
+  	jobs[0];
+  end
+
+  # return's an array the size of the hyper period
+  # each slot ether has nil for idle or the name of the task executing
+  def generate_schedule
+  	schedule = Array.new
+  	queue = Array.new
+  	current_job = nil
+  	current_job_execution_time = 0
+
+  	for time in 0...hyper_period
+  		# add released tasks to queue
+  		queue += get_released_jobs time
+
+  		# give the current job credit for execution in last step
+  		current_job_execution_time += 1 unless current_job.nil?
+
+  		#check if current task is finished
+  		if !!current_job and current_job[:task].worst_case_execution_time == current_job_execution_time
+  			#task is finished
+  			current_job = nil
+  			current_job_execution_time = 0
+  		end
+
+  		#find a task to execute
+  		if current_job.nil?
+  			current_job = get_highest_priorty_task(queue)
+  			queue.delete(current_job)
+  		end
+
+  		# if we are missing a deadline task set is not schedulable
+  		if !!current_job and time >= current_job[:deadline]
+  			return nil
+  		end
+
+  		# add current job to schedule
+  		schedule.push(current_job.nil? ? nil : current_job[:task].name)
+
+  		#log whats executing in this slot
+  		#puts "Slot #{time + 1} " + (current_job.nil? ? "Idle" : current_job[:task].name)
+  	end
+
+  	# if queue isn't empty, then return nil
+  	return nil unless queue.empty?
+
+  	#return schedule
+  	schedule
+  end
+
+  def verify(schedule)
+  	# basic sanity check. does the provided schedule equal the hyperperiod?
+  	if(schedule.count != hyper_period)
+  		raise "Provided schedule not equal to hyper-period of #{hyper_period}"
+  	end
+
+  	#verify each task in the set
+  	self.tasks.each do |task|
+  		verify_task(schedule, task)
+  	end
+  end
+
+  def verify_task(schedule, task)
+  	#slice the schedule into periods for the task
+  	schedule.each_slice(task.period) do |period|
+
+  		# get all occurence of task in period
+  		tasks = period.select{|t| t == task.name}
+  		
+  		# make sure schedule has given task enough time to complete (WCET)
+  		unless tasks.count == task.worst_case_execution_time
+  			raise "Task #{task.name} not given enough execution time" 
+  		end
+
+  		# make sure last occurence of task in period is before deadline
+  		unless period.rindex(task.name) <= task.deadline
+  			raise "Task #{task.name} missed deadline"
+  		end
+  	end
+  end
+
 end
--- test/unit/task_set_test.rb
+++ test/unit/task_set_test.rb
@@ -1,7 +1,177 @@
 require 'test_helper'
 
 class TaskSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "Calculate Hyper Period" do
+		ts = TaskSet.create(:name => "Silly Task Set",
+     :tasks => [Task.new(:period => 5),
+      Task.new(:period => 10),
+      Task.new(:period => 15)])
+
+		assert_equal "Silly Task Set", ts.name
+		assert_equal 30, ts.hyper_period
+  end
+
+  test "Find highest priority task in the queue" do
+    medium = Task.new(:period => 5, :deadline => 5)
+    low = Task.new(:period => 10, :deadline => 10)
+    high = Task.new(:period => 5, :deadline => 3)
+
+    ts = TaskSet.new
+
+  		# the get_highest_priority_task method takes an array of hashes
+  		# try different orders to make sure order doesn't matter
+  		queue_one = [{:task => high}, {:task => medium}, {:task => low}]
+  		queue_two = [{:task => low}, {:task => high}, {:task => medium}]
+  		queue_three = [{:task => medium}, {:task => low}, {:task => high}]
+
+  		assert_equal high, ts.get_highest_priorty_task(queue_one)[:task]
+  		assert_equal high, ts.get_highest_priorty_task(queue_two)[:task]
+  		assert_equal high, ts.get_highest_priorty_task(queue_three)[:task]
+  end
+
+	test "Release of tasks" do
+		ts = TaskSet.new(:name => "Awesome Task Set")
+		low = Task.new(:period => 10, :deadline => 10, :offset => 0)
+		medium = Task.new(:period => 5, :deadline => 5, :offset => 0)
+		high = Task.new(:period => 5, :deadline => 3, :offset => 0)
+		ts.tasks = [low, medium, high]
+
+		ts.save
+
+		# tasks should be rleased at 0 5 10 15 20
+		
+		# time 0 - all jobs should be released
+		released_tasks = ts.get_released_jobs(0)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 3, tasks.count
+		assert tasks.include?(medium)
+		assert tasks.include?(low)
+		assert tasks.include?(high)
+
+		# time 2 - Nothing released
+		released_tasks = ts.get_released_jobs(2)
+		assert_empty(released_tasks)
+
+		#time 5 -medium and high released
+		released_tasks = ts.get_released_jobs(5)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 2, tasks.count
+		assert tasks.include?(medium)
+		assert tasks.include?(high)
+
+		# time 7 - Nothing released
+		released_tasks = ts.get_released_jobs(7)
+		assert_empty(released_tasks)
+
+		# time 10 - all jobs should be released
+		released_tasks = ts.get_released_jobs(10)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 3, tasks.count
+		assert tasks.include?(medium)
+		assert tasks.include?(low)
+		assert tasks.include?(high)
+
+		# time 12 - Nothing released
+		released_tasks = ts.get_released_jobs(12)
+		assert_empty(released_tasks)
+
+		#time 15 -medium and high released
+		released_tasks = ts.get_released_jobs(15)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 2, tasks.count
+		assert tasks.include?(medium)
+		assert tasks.include?(high)
+
+		# time 17 - Nothing released
+		released_tasks = ts.get_released_jobs(17)
+		assert_empty(released_tasks)
+
+		# time 20 - all jobs should be released
+		released_tasks = ts.get_released_jobs(20)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 3, tasks.count
+		assert tasks.include?(medium)
+		assert tasks.include?(low)
+		assert tasks.include?(high)
+	end
+
+	test "Release with offset" do
+		ts = TaskSet.new(:name => "Awesome Task Set")
+		task = Task.new(:period => 10, :deadline => 10, :offset => 3)
+		ts.tasks = [task]
+
+		ts.save
+
+		# task should be released at 3, 13, 23
+
+		# time 0 - Nothing released
+		released_tasks = ts.get_released_jobs(0)
+		assert_empty(released_tasks)
+
+		# time 3
+		released_tasks = ts.get_released_jobs(3)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 1, tasks.count
+		assert tasks.include?(task)
+
+		# time 10 - Nothing released
+		released_tasks = ts.get_released_jobs(10)
+		assert_empty(released_tasks)
+
+		# time 13
+		released_tasks = ts.get_released_jobs(13)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 1, tasks.count
+		assert tasks.include?(task)
+
+		# time 20 - Nothing released
+		released_tasks = ts.get_released_jobs(10)
+		assert_empty(released_tasks)
+
+		# time 23
+		released_tasks = ts.get_released_jobs(23)
+		tasks = released_tasks.map{|t| t[:task]}
+		assert_equal 1, tasks.count
+		assert tasks.include?(task)
+	end
+
+	test "Fail impossible schedule" do
+		ts = TaskSet.new(:name => "Awesome Task Set")
+		medium = Task.new(:period => 10, :worst_case_execution_time => 5,
+     :deadline => 10, :offset => 0,
+     :name => "Task Medium")
+		high = Task.new(:period => 10, :worst_case_execution_time => 5,
+     :deadline => 5, :offset => 0,
+     :name => "Task High")
+		low = Task.new(:period => 10, :worst_case_execution_time => 1, 
+     :deadline => 10, :offset => 0,
+     :name => "Task Low")
+		ts.tasks = [low, medium, high]
+
+		ts.save
+
+		schedule = ts.generate_schedule
+
+		assert_nil schedule
+	end
+
+	test "Schedule simple schedule" do
+		ts = TaskSet.new(:name => "Awesome Task Set")
+		task_one = Task.new(:period => 10, :worst_case_execution_time => 5,
+     :deadline => 10, :offset => 0,
+     :name => "Task One")
+		task_two = Task.new(:period => 10, :worst_case_execution_time => 5,
+     :deadline => 5, :offset => 0,
+     :name => "Task Two")
+		ts.tasks = [task_one, task_two]
+
+		ts.save
+
+		expected_schedule = ["Task Two", "Task Two", "Task Two", "Task Two", "Task Two",
+      "Task One", "Task One", "Task One", "Task One", "Task One"]
+      schedule = ts.generate_schedule
+
+      assert_equal expected_schedule, schedule
+  end
+  
 end
