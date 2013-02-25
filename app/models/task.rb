1fa05fd588099049193e54d447a3fa52fda2670a
Ray Tiley <raytiley@gmail.com>
2013-02-25 11:44:36 -0600

    Created a statice idle task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,10 @@ class Task < ActiveRecord::Base
   attr_accessible :deadline, :name, :offset, :period, :worst_case_execution_time
 
   belongs_to :task_set
+
+  def self.idle
+  	t = Task.new(:name => "Idle", :period =>1)
+  	t.id = 0
+  	t
+  end
 end
