b6e2e20ed367c0f8d74a695d1d0bc894eac33a53
Ray Tiley <raytiley@gmail.com>
2013-02-28 09:05:13 -0600

    Massage the data to prevent weird stuff
    Always make sure that period offset deadline, and wcet are set initialized to at least 0
    
    Ensure that deadline or wcet aren't longer than period

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,20 @@
 class Task < ActiveRecord::Base
   attr_accessible :deadline, :name, :offset, :period, :worst_case_execution_time, :task_set_id
-
   belongs_to :task_set
+  before_save :massage_data
 
   def self.idle
   	t = Task.new(:name => "Idle", :period =>1)
   	t.id = 0
   	t
+  end
+
+  def massage_data
+  	self.offset ||= 0
+  	self.period ||= 0
+  	self.worst_case_execution_time ||=0
+  	self.worst_case_execution_time = self.period if self.worst_case_execution_time > self.period
+  	self.deadline ||= self.period
+  	self.deadline = self.period if self.deadline > self.period || self.deadline = 0
   end
 end
